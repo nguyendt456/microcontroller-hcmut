f65992524cb267759c379de54d8b96bb94149774
nguyendt456 <nguyendt456@gmail.com>
2022-11-18 15:54:31 +0700

    Lab 2 done

---
+++ Lab2/Exercise3/Core/Inc/main.h
@@ -0,0 +1,94 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define DOT_Pin GPIO_PIN_4
+#define DOT_GPIO_Port GPIOA
+#define LED_RED_Pin GPIO_PIN_5
+#define LED_RED_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_6
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_7
+#define EN1_GPIO_Port GPIOA
+#define _a_Pin GPIO_PIN_0
+#define _a_GPIO_Port GPIOB
+#define _b_Pin GPIO_PIN_1
+#define _b_GPIO_Port GPIOB
+#define _c_Pin GPIO_PIN_2
+#define _c_GPIO_Port GPIOB
+#define EN2_Pin GPIO_PIN_8
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_9
+#define EN3_GPIO_Port GPIOA
+#define _d_Pin GPIO_PIN_3
+#define _d_GPIO_Port GPIOB
+#define _e_Pin GPIO_PIN_4
+#define _e_GPIO_Port GPIOB
+#define _f_Pin GPIO_PIN_5
+#define _f_GPIO_Port GPIOB
+#define _g_Pin GPIO_PIN_6
+#define _g_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Lab2/Exercise3/Core/Inc/software_timer.h
@@ -0,0 +1,20 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Nov 4, 2022
+ *      Author: nguye
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+extern int timer2_flag;
+extern int timer3_flag;
+
+void setTimer1(int duration);
+void setTimer2(int duration);
+void setTimer3(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
